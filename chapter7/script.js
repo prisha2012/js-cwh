3011d2935d7c0e0e67d1b604ca70d508baf97126
Prisha <153307711+prisha2012@users.noreply.github.com>
2026-04-29 23:21:07 +0530

    new

---
+++ chapter7/script.js
@@ -0,0 +1,13 @@
+// const changeBodyRed=()=>{
+//     document.body.firstElementChild.style.background="red";
+// }
+// let b=document.body
+// console.log("first child of b is",b.firstChild);
+// console.log("first child of b is",b.firstElementChild);
+// let t=document.body.firstElementChild.firstElementChild;
+// console.log(t);
+// console.log(t.rows);
+// console.log(t.rows[0]);
+// console.log(t.rows[0].cells);
+let ctitile=document.getElementsByClassName("container")[0];
+ctitile.style.color="red";
