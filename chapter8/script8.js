f0f79403c358a3f90bda124841a3d85ec346cd1a
Prisha <153307711+prisha2012@users.noreply.github.com>
2026-04-30 00:15:55 +0530

    new

---
+++ chapter8/script8.js
@@ -0,0 +1,9 @@
+// myspan.innerHTML="hello world";
+// let a=first.getAttribute("class");
+// console.log(a);
+// let a=doxument.getElementsByTagName("div")[0]
+//  a.innerHTML=a.innerHTML+"<h1>hello world</h1>";
+
+let div=document.createElement('div');
+div.innerHTML="<h1>hello world</h1>";
+a.appendChild(div);
---
+++ practise7.js
@@ -0,0 +1,5 @@
+document.getElementsByTagName("nav")[0].firstElementChild.style.color="green";
+Array.from(document.getElementsByTagName("li")).forEach(element => {
+    element.style.color="red";
+    
+});
