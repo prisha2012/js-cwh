2a7695584065a043679dfbbcb98a48956ec6fe16
Prisha <153307711+prisha2012@users.noreply.github.com>
2026-05-11 20:55:28 +0530

    new

---
+++ AfterMath/96_modules.js
@@ -0,0 +1,9 @@
+// console.log("i am good");
+// var pdf=require("pdf--node");
+// var fs=require("fs");
+
+// var html=fs.readFileSync("template.html");
+
+const regex=/very/g;
+const text="Harry is a very very nice awsome nice very boy";
+console.log(text.replace(regex,"VERY"));
